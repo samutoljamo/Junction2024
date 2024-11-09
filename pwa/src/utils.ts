f3eac7ee64ad9ba3c0714fe50165b08fda9c9537
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-11-09 22:25:24 +0200

    make utils importable

--- pwa/src/utils.ts
+++ pwa/src/utils.ts
@@ -6,7 +6,10 @@
  * @param lon - The longitude of the point to transform.
  * @returns An object containing the normalized coordinates x and y.
  */
-function gpsToNormalized(lat: number, lon: number): { x: number; y: number } {
+export function gpsToNormalized(
+  lat: number,
+  lon: number
+): { x: number; y: number } {
   const top_left = { lat: 60.16241, lon: 24.905575 }; // Top-left corner
   const top_right = { lat: 60.16186, lon: 24.905884 }; // Top-right corner
   const bottom_right = { lat: 60.161362, lon: 24.902548 }; // Bottom-right corner
