12024066158e30ac7b3aefe29e22624c0694a27a
cfjale <jani.lehto@tuni.fi>
2024-11-09 13:10:34 +0200

    merg

--- pwa/src/routes/bottom-bar.tsx
+++ pwa/src/routes/bottom-bar.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Button, Grid } from '@mui/joy';
 
-import KeyboardArrowRight from '@mui/icons-joy/KeyboardArrowRight';
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+
 
 export default function Bottombar() {
     const navigate = useNavigate();
