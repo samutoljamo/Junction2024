6430e40078624bb20d64530c24819b11ac73cb7e
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-11-09 13:03:09 +0200

    Fix project

--- pwa/src/assets/components/CameraView.tsx
+++ pwa/src/assets/components/CameraView.tsx
@@ -1,47 +1,57 @@
 import { Camera } from "react-camera-pro";
-import { useRef } from 'react';
-import { Button, Stack, Typography, Grid, Box } from '@mui/joy';
+import { useRef } from "react";
+import { Button, Stack, Typography, Grid, Box } from "@mui/joy";
 import { useAppSelector, useAppDispatch } from "../../store";
 import { addImage, removeImage } from "../../formSlice";
+import { NavLink } from "react-router-dom";
 
 export default function CameraView() {
   const camera = useRef(null);
   const images = useAppSelector((state) => state.form.images);
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   return (
-    <Stack spacing={2} alignItems="center" sx={{ width: '100%', mx: 'auto', p: 2 }}>
+    <Stack
+      spacing={2}
+      alignItems="center"
+      sx={{ width: "100%", mx: "auto", p: 2 }}
+    >
       <Camera ref={camera} errorMessages={{}} aspectRatio={1} />
       <Typography>Images</Typography>
-        <Grid container spacing={1} justifyContent="center">
+      <Grid container spacing={1} justifyContent="center">
         {images.map((image, index) => (
-          <Grid xs={4} key={index} onClick={() => {
-            dispatch(removeImage(image.id))
-          }}>
-          <img 
-            src={image.data} 
-            alt={`Captured ${index}`} 
-            style={{
-              width: '100%',
-              borderRadius: '8px',
-            }} 
-          />
-        </Grid>
+          <Grid
+            xs={4}
+            key={index}
+            onClick={() => {
+              dispatch(removeImage(image.id));
+            }}
+          >
+            <img
+              src={image.data}
+              alt={`Captured ${index}`}
+              style={{
+                width: "100%",
+                borderRadius: "8px",
+              }}
+            />
+          </Grid>
         ))}
-        </Grid>
+      </Grid>
 
-     <Box flexGrow={1} />
+      <Box flexGrow={1} />
       <Button
         disabled={images.length > 2}
         onClick={() => {
           if (camera.current) {
             const capturedImage = camera.current.takePhoto();
-            dispatch(addImage(capturedImage))
+            dispatch(addImage(capturedImage));
           }
         }}
       >
         Capture Image
       </Button>
+      <NavLink to="/map">Next</NavLink>
     </Stack>
   );
 }
--- pwa/src/routes/bottom-bar.tsx
+++ pwa/src/routes/bottom-bar.tsx
@@ -1,74 +1,74 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, Button, Grid } from '@mui/joy';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, Grid } from "@mui/joy";
 
-import KeyboardArrowRight from '@mui/icons-joy/KeyboardArrowRight';
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 
 export default function Bottombar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const routes = ["/map", "/", "/confirmdata", "/confirmcondition"];
-    
-    // Use state to track the current index in the route list
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const routes = ["/map", "/", "/confirmdata", "/confirmcondition"];
 
-    const handleNextRoute = () => {
-        // Increment the index and handle loop around
-        const nextIndex = (currentIndex + 1) % routes.length; // This ensures it loops back to the first route
-        setCurrentIndex(nextIndex); // Update the state with the next index
+  // Use state to track the current index in the route list
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-        // Navigate to the next route
-        navigate(routes[nextIndex]);
-    };
+  const handleNextRoute = () => {
+    // Increment the index and handle loop around
+    const nextIndex = (currentIndex + 1) % routes.length; // This ensures it loops back to the first route
+    setCurrentIndex(nextIndex); // Update the state with the next index
 
-    const handlePreviousRoute = () => {
-        // Decrement the index and handle loop around (if going back from the first route)
-        const prevIndex = (currentIndex - 1 + routes.length) % routes.length; // This ensures it loops to the last route
-        setCurrentIndex(prevIndex); // Update the state with the previous index
+    // Navigate to the next route
+    navigate(routes[nextIndex]);
+  };
 
-        // Navigate to the previous route
-        navigate(routes[prevIndex]);
-    };
+  const handlePreviousRoute = () => {
+    // Decrement the index and handle loop around (if going back from the first route)
+    const prevIndex = (currentIndex - 1 + routes.length) % routes.length; // This ensures it loops to the last route
+    setCurrentIndex(prevIndex); // Update the state with the previous index
 
-    return (
-        <Box
+    // Navigate to the previous route
+    navigate(routes[prevIndex]);
+  };
+
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        bgcolor: "black",
+        borderTop: "1px solid",
+        borderColor: "divider",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid xs={6} container justifyContent="center" alignItems="center">
+          <Button
+            onClick={handlePreviousRoute} // Use the backward navigation here
+            variant="solid"
             sx={{
-                position: 'fixed',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                bgcolor: 'black',
-                borderTop: '1px solid',
-                borderColor: 'divider',
+              margin: 2,
+              minWidth: "150px", // Set minimum width here
             }}
-        >
-            <Grid container spacing={2}>
-            <Grid item xs={6} container justifyContent="center" alignItems="center">
-                    <Button
-                        onClick={handlePreviousRoute} // Use the backward navigation here
-                        variant="solid"
-                        sx={{
-                            margin: 2,
-                            minWidth: '150px', // Set minimum width here
-                        }}
-                    >
-                        Back
-                    </Button>
-                </Grid>
-                <Grid item xs={6} container justifyContent="center" alignItems="center">
-                    <Button
-                        onClick={handleNextRoute}
-                        variant="solid"
-                        endDecorator={<KeyboardArrowRight />}
-                        sx={{
-                            margin: 2,
-                            minWidth: '150px', // Set minimum width here
-                        }}
-                    >
-                        Next
-                    </Button>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+          >
+            Back
+          </Button>
+        </Grid>
+        <Grid xs={6} container justifyContent="center" alignItems="center">
+          <Button
+            onClick={handleNextRoute}
+            variant="solid"
+            endDecorator={<KeyboardArrowRight />}
+            sx={{
+              margin: 2,
+              minWidth: "150px", // Set minimum width here
+            }}
+          >
+            Next
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
