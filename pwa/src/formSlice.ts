c33a72f7d0bcc0a647722912c181f5a8d25713ca
Otto Haarahiltunen <otto.haarahiltunen@tuni.fi>
2024-11-09 12:53:17 +0200

    curr

--- pwa/src/formSlice.ts
+++ pwa/src/formSlice.ts
@@ -1,23 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit'
 interface FormSlice {
-    images: string[];
+    images: ({
+        data: string,
+        id: number;
+    })[];
 }
 
 const initialState: FormSlice = {
     images: [],
 }
+let id = 0;
 export const formSlice = createSlice({
   name: 'form',
   initialState,
   reducers: {
     addImage: (state, action) => {
-      state.images.push(action.payload)
+      state.images.push({
+        data: action.payload,
+        id: id++,
+      })
     },
-
+    removeImage: (state, action) => {
+        state.images = state.images.filter(image => image.id !== action.payload)
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addImage } = formSlice.actions
+export const { addImage, removeImage } = formSlice.actions
 
 export default formSlice.reducer
